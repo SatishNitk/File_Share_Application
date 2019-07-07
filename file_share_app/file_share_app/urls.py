5a53f6d83c0c062386e7d19c8a00c9aa5bd4eefe
satish04121995 <satishkrgu95@gmail.com>
2019-07-07 16:30:49 +0530

    setting fil url and admin

--- file_share_app/file_share_app/urls.py
+++ file_share_app/file_share_app/urls.py
@@ -1,4 +1,4 @@
-"""file_share_app URL Configuration
+"""file_share_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
+from accounts.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('account/',include('accounts.urls')),
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
